95a553ebdec728192305e1325583551c949935d1
pistomat <pistomat@users.noreply.github.com>
2023-03-21 16:59:21 +0100

    extract updated FullMath an TickMath libraries

--- src/Contracts/puppet-v3/OracleLibrary.sol
+++ src/Contracts/puppet-v3/OracleLibrary.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity ^0.8.0;
 
-import '@uniswap/v3-core/contracts/libraries/FullMath.sol';
-import '@uniswap/v3-core/contracts/libraries/TickMath.sol';
+import './FullMath.sol';
+import './TickMath.sol';
 import '@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol';
 
 /// @title Oracle library
